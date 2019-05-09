9782251c8b4f9362cdda7c86506748351f13f73f
kristjankivikangur <kristjan.kivikangur@tthk.ee>
2019-05-09 12:29:26 +0300

    Loo LaeKangelased meetod

--- Kangelased/Kangelased/Program.cs
+++ Kangelased/Kangelased/Program.cs
@@ -8,6 +8,25 @@ namespace Kangelased
 {
 	class Program
 	{
+		static List<Kangelane> Kangelased=new List<Kangelane>();
+		static void LaeKangelased(string Failinimi)
+		{
+			string[] failiAndmed = System.IO.File.ReadAllLines(Failinimi);
+			for (int i = 0; i < failiAndmed.Length; i++)
+			{
+				string[] nimiKoht = failiAndmed[i].Split(new char[] { '/' });
+				string nimi = nimiKoht[0].Trim();
+				string asukoht = nimiKoht[1].Trim();
+				if (nimi.EndsWith("*"))
+				{
+					Kangelased.Add(new SuperKangelane(nimi.Trim(new char[] { '*' }), asukoht));
+				}
+				else
+				{
+					Kangelased.Add(new Kangelane(nimi, asukoht));
+				}
+			}
+		}
 		static void Main(string[] args)
 		{
 		}
